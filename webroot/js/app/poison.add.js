6929e00eedc9565043c47a7beb86be5dfa6aa94f
margarinas <LZ@LZ-PC.(none)>
2013-10-05 01:08:09 +0300

    fixed multiple tinymce instances

--- webroot/js/app/poison.add.js
+++ webroot/js/app/poison.add.js
@@ -1,7 +1,12 @@
 define(["app/substance/add","app/agent/add","app/substance/actions","common","purl"],function(substances,agents,actions){
 
-	poison = $.url().segment(-2);
-	eval(poison).init();
+	poison_url = $.url().attr("directory");
+	poison = poison_url.split("/");
+	console.log(poison.indexOf("substances"));
+	if(poison.indexOf("substances")!==-1)
+		substances.init();
+	else if (poison.indexOf("agents") !==-1)
+		agents.init();
 	// substance.init();
 	// agent.init();
 	actions.init();
